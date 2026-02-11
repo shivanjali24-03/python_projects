b1749a88575bf3b2b076c889fe9cdc8e329193b8
shivanjali24-03 <156819273+shivanjali24-03@users.noreply.github.com>
2026-02-12 00:52:41 +0530

    Day1

---
+++ Day1/day1-arithmatic.py
@@ -0,0 +1,13 @@
+#Take two numbers and print their sum, difference, multiplication, division.
+
+a=int(input("Enter a Number : "))
+b=int(input("Enter b Number :"))
+
+#sum of two numbers
+print("Sum of Two numbers", a+b)
+#Multiplication
+print("Multiplication of Two numbers", a*b)
+#division
+print("Division of Two numbers", a/b)
+#Mod
+print("Division of Two numbers", a%b)
---
+++ Day1/day1-swapNumbers.py
@@ -0,0 +1,11 @@
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+
+# Swap using arithmetic
+a = a + b
+b = a - b
+a = a - b
+
+print("After swap:")
+print("a =", a)
+print("b =", b)
