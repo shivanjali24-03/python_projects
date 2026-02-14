2e87b4e9a15967e4a16974f9fc865f44984e3132
shivanjali24-03 <156819273+shivanjali24-03@users.noreply.github.com>
2026-02-14 23:59:22 +0530

    day3-sets

---
+++ Day3/day3-all-zero-to-end.py
@@ -0,0 +1,15 @@
+nums = [1, 0, 3, 0, 5, 2]
+
+result = []
+zero_count = 0
+
+for x in nums:
+    if x != 0:
+        result.append(x)
+    else:
+        zero_count += 1
+
+for _ in range(zero_count):
+    result.append(0)
+
+print(result)
